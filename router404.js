8cd3cb4442de9d972f14638e7a17c0646895969f
zxck / waefur <datafetchsyserror@gmail.com>
2025-11-18 22:58:56 +0800

    bit big changes
    uh

--- router404.js
+++ router404.js
@@ -4,7 +4,10 @@ const route = async () => {
         if (!paige.ok) {
             const paige2 = await fetch(window.location.href + window.location.pathname.match(/(?:\/[^\/]+)($)/) + '.html')
             if (!paige.ok) {
-                throw new Error(`No page found here! Status: ${paige.status}, ${paige2.status}`);
+                const paige3 = await fetch(window.location.href + '.html')
+                if (!paige.ok) {
+                    throw new Error(`No page found here! Status: ${paige.status}, ${paige2.status}, ${paige3.status}`);
+                }
             }
         }
         document.documentElement.innerHTML = paige.text();
