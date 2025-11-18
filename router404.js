a265532452131a49f769bda5f4da0e48f22de134
zxck / waefur <datafetchsyserror@gmail.com>
2025-11-18 23:13:28 +0800

    bad routing
    skrunglky

--- router404.js
+++ router404.js
@@ -2,15 +2,16 @@ const route = async () => {
     try {
         const paige = await fetch(window.location.href + '/index.html')
         if (!paige.ok) {
-            const paige2 = await fetch(window.location.href + window.location.pathname.match(/(?:\/[^\/]+)($)/) + '.html')
+            paige = await fetch(window.location.href + window.location.pathname.match(/(?:\/[^\/]+)($)/) + '.html')
             if (!paige.ok) {
-                const paige3 = await fetch(window.location.href + '.html')
+                paige = await fetch(window.location.href + '.html')
                 if (!paige.ok) {
-                    throw new Error(`No page found here! Status: ${paige.status}, ${paige2.status}, ${paige3.status}`);
+                    throw new Error(`No page found here! Status: ${paige.status}`);
                 }
             }
         }
-        document.documentElement.innerHTML = paige.text();
+        window.location.replace(paige.url); // goodbye!!
+        // document.documentElement.innerHTML = paige.text();
     } catch (error) {
         console.error(error.message);
     }
