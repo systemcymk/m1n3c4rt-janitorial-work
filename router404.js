7988dea79d0453a2a29bcae8b4323f236b5d7d23
zxck / waefur <datafetchsyserror@gmail.com>
2025-11-18 23:21:49 +0800

    t

--- router404.js
+++ router404.js
@@ -10,8 +10,8 @@ const route = async () => {
                 }
             }
         }
-        window.location.replace(paige.url);
-        window.location.reload(); // goodbye!!
+        window.location.assign(paige.url);
+        // window.location.reload(); // goodbye!!
         // document.documentElement.innerHTML = paige.text();
     } catch (error) {
         console.error(error.message);
